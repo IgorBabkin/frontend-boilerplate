77816d95a5ffa5563a070f1d557e2beaae82186f
ibabkin <babkin.i@obelis.ltd>
2024-04-07 15:28:18 +0200

    feat(error handler): add useAsyncEventHandler

--- src/app/widgets/todo/AddTodoFormWidget.tsx
+++ src/app/widgets/todo/AddTodoFormWidget.tsx
@@ -1,26 +1,21 @@
 import { FormEvent, useCallback, useState } from 'react';
 import Button from '../../ui/button/Button.tsx';
 import TextField from '../../ui/textField/TextField.tsx';
-import { IErrorHandler, IErrorHandlerKey } from '../../domain/errors/IErrorHandler.ts';
-import { useDependency } from '../../../lib/scope/ScopeContext.ts';
-import { useService } from '../../../lib/scope/useQuery.ts';
+import { useAsyncEventHandler, useService } from '../../../lib/scope/useQuery.ts';
 import { ITodoServiceKey, TodoService } from './TodoService.ts';
 
 function AddTodoFormWidget() {
   const todoService = useService<TodoService>(ITodoServiceKey);
-  const errorHandler = useDependency<IErrorHandler>(IErrorHandlerKey);
   const [title, setTitle] = useState('');
   const resetForm = useCallback(() => setTitle(''), []);
 
-  const submit = useCallback(
-    (e: FormEvent) => {
-      errorHandler.handle(async () => {
-        e.preventDefault();
-        await todoService.addTodo(title);
-        resetForm();
-      });
+  const submit = useAsyncEventHandler(
+    async (e: FormEvent) => {
+      e.preventDefault();
+      await todoService.addTodo(title);
+      resetForm();
     },
-    [errorHandler, resetForm, title, todoService],
+    [resetForm, title, todoService],
   );
 
   return (
--- src/lib/scope/useQuery.ts
+++ src/lib/scope/useQuery.ts
@@ -1,5 +1,5 @@
 import { InjectionToken } from 'ts-ioc-container';
-import { useEffect, useMemo } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
 import { useDependency } from './ScopeContext.ts';
 import { IErrorBus, IErrorBusKey } from '../../app/domain/errors/ErrorBus.ts';
 import { IMediator } from '../mediator/IMediator.ts';
@@ -55,4 +55,14 @@ export const useAsyncEffect = (fn: () => Promise<void>, deps: unknown[]) => {
   useEffect(() => {
     fn().catch((e) => errorBus$.next(e));
   }, deps);
+};
+
+export const useAsyncEventHandler = <T>(fn: (e: T) => Promise<void>, deps: unknown[]) => {
+  const errorBus$ = useDependency<IErrorBus>(IErrorBusKey);
+  return useCallback(
+    (e: T) => {
+      fn(e).catch((error) => errorBus$.next(error));
+    },
+    [errorBus$, ...deps],
+  );
 };
