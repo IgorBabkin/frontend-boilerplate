d721d5ca13907297e78a2e128eed7262ce81e97d
ibabkin <babkin.i@obelis.ltd>
2024-04-11 18:15:06 +0200

    refactor(sad): asd

--- src/env/Common.ts
+++ src/env/Common.ts
@@ -3,22 +3,22 @@ import { IErrorBusKey } from '@domain/errors/ErrorBus.ts';
 import { TodoStore } from '@domain/todo/TodoStore.ts';
 import { UserStore } from '@domain/user/UserStore.ts';
 import { Subject } from 'rxjs';
-import { ServiceMediator } from '../lib/mediator/ServiceMediator.ts';
-import { CheckPermission } from '../app/widgets/auth/CheckPermission.ts';
+import { ServiceMediator } from '@lib/mediator/ServiceMediator.ts';
+import { CheckPermission } from '@widgets/auth/CheckPermission.ts';
 import { ErrorHandler } from '@domain/errors/IErrorHandler.ts';
 import { TodoRepo } from '@domain/todo/TodoRepo.ts';
-import { hasTags } from '../lib/scope/container.ts';
+import { hasTags } from '@lib/scope/container.ts';
 import { UserRepo } from '@domain/user/UserRepo.ts';
 import { AuthClient, IAuthClientKey } from '../app/api/AuthClient.ts';
-import { Context } from '../lib/scope/Context.ts';
+import { Context } from '@lib/scope/Context.ts';
 import { AuthProvider } from '@domain/auth/AuthProvider.ts';
 import { ApiClient, IApiClientKey } from '../app/api/ApiClient.ts';
-import { TodoService } from '../app/widgets/todo/TodoService.ts';
-import { UserService } from '../app/widgets/auth/UserService.ts';
-import { ErrorService } from '../app/widgets/errors/ErrorService.ts';
-import { NotificationStore } from '../app/widgets/notifications/NotificationStore.ts';
-import { NotificationService } from '../app/widgets/notifications/NotificationService.ts';
-import { AuthService } from '../app/widgets/auth/AuthService.ts';
+import { TodoService } from '@widgets/todo/TodoService.ts';
+import { UserService } from '@widgets/auth/UserService.ts';
+import { ErrorService } from '@widgets/errors/ErrorService.ts';
+import { NotificationStore } from '@widgets/notifications/NotificationStore.ts';
+import { NotificationService } from '@widgets/notifications/NotificationService.ts';
+import { AuthService } from '@widgets/auth/AuthService.ts';
 
 export class Common implements IContainerModule {
   applyTo(container: IContainer): void {
