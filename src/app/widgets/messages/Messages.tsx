2ecae008b49f6e2be9dbd4677d7bb79dc8efe80e
ibabkin <babkin.i@obelis.ltd>
2024-04-11 02:04:05 +0200

    refactor(subscribe): asdas

--- src/app/widgets/messages/Messages.tsx
+++ src/app/widgets/messages/Messages.tsx
@@ -1,11 +1,11 @@
 import { useObservable } from '@lib/observable/observable.ts';
 import { useDependency } from '@lib/scope/ScopeContext.ts';
-import { IMessageServiceKey } from '../messages/MessageService.ts';
+import { IMessageServiceKey } from './MessageService.ts';
 
-function ErrorsWidget() {
+function Messages() {
   const messageService = useDependency(IMessageServiceKey.resolve);
   const message = useObservable(() => messageService.getMessage$(), undefined, [messageService]);
   return <div>{message}</div>;
 }
 
-export default ErrorsWidget;
+export default Messages;
